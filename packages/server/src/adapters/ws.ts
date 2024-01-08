0a3fc9a6da7ea974969184412532c496ff7c50b2
Gabriel Pichot <gabriel.pichot@gmail.com>
2024-01-08 19:12:31 +0100

    fix: reference to websocket open state

--- packages/server/src/adapters/ws.ts
+++ packages/server/src/adapters/ws.ts
@@ -159,7 +159,7 @@ export function applyWSSHandler<TRouter extends AnyRouter>(
           },
         });
         /* istanbul ignore next -- @preserve */
-        if (client.readyState !== client.OPEN) {
+        if (client.readyState !== WebSocket.OPEN) {
           // if the client got disconnected whilst initializing the subscription
           // no need to send stopped message if the client is disconnected
           sub.unsubscribe();
@@ -294,7 +294,7 @@ export function applyWSSHandler<TRouter extends AnyRouter>(
       };
       const data = JSON.stringify(response);
       for (const client of wss.clients) {
-        if (client.readyState === 1 /* ws.OPEN */) {
+        if (client.readyState === WebSocket.OPEN) {
           client.send(data);
         }
       }
